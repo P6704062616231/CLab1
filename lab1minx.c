d98640f9abe75f2876b87c4de161a9f76e8871e4
6704062616231 <s6704062616231@email.kmutnb.ac.th>
2024-11-28 18:24:52 +0700

    change input

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main() {
-	int num1=100;
-	int num2=50;
+	int num1;
+	int num2;
+	printf("n1: and n2:");
+	scanf("%d" "%d",&num1 , &num2);
+
 	if (num1 > num2){
 		printf("num1 is greater than num2");
+		printf("\n%d",(num1-num2));
 	}	
 	else{
 		printf("num2 is greater than num1");
+		printf("\n%d",(num2-num1));
 	}
 }
